f3a4aed8e0d35a338bf2e1ebc550c34b9de8fe61
bj0rge <bastien.jorge@gmail.com>
2019-12-03 17:47:11 +0100

    feat: add pin tabs on forest script

---
+++ scripts/pin-tabs-on-forest.js
@@ -0,0 +1,124 @@
+// ==UserScript==
+// @name         Pin tabs on forest
+// @namespace    https://spendesk.com
+// @updateURL    https://raw.githubusercontent.com/Spendesk/tampermonkey-scripts/master/scripts/pin-tab-forest.js
+// @downloadURL  https://raw.githubusercontent.com/Spendesk/tampermonkey-scripts/master/scripts/pin-tab-forest.js
+// @version      0.1
+// @description  Add ability to pin tabs on forest in the sidebar
+// @author       Spendesk
+// @match        *://app.forestadmin.com/*
+// @icon         https://www.spendesk.com/favicon-32x32.png
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_addStyle
+
+// ==/UserScript==
+
+GM_addStyle('.spendesk-not-fav, .spendesk-fav { color: gold !important; }');
+GM_addStyle(
+  '.spendesk-not-fav:hover, .spendesk-fav:hover { color: goldenrod !important; }'
+);
+GM_addStyle(
+  '.spendesk-fav-button { background: transparent; border: none; font-size: 8px; padding: 3px; position: absolute; right: 2px; top: 12px; }'
+);
+GM_addStyle('.spendesk-fav-button:hover { cursor: pointer; }');
+
+const DATA_ATTRIBUTE = 'data-pinned';
+const NOT_FAVORITE_ICON =
+  '<i class="fa fa-star-o fa-2x spendesk-not-fav" role="button"></i>';
+const FAVORITE_ICON =
+  '<i class="fa fa-star fa-2x spendesk-fav" role="button"></i>';
+
+const wait = (timeout) =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(), timeout);
+  });
+
+const getSidebar = () => document.querySelector('.l-sidebar');
+
+const getSections = (sidebar) =>
+  Array.from(sidebar.querySelectorAll('.c-side-menu__list>.ember-view'));
+
+const getSectionName = (section) =>
+  section.querySelector('.c-side-menu-item__name').textContent.trim();
+
+const getIcon = (isFilled) => (isFilled ? FAVORITE_ICON : NOT_FAVORITE_ICON);
+
+const getIsButtonCurrentlyPinned = (button) =>
+  button.getAttribute(DATA_ATTRIBUTE) === '1';
+
+const appendIcon = (button, isPinned) => {
+  const icon = getIcon(isPinned);
+  button.innerHTML = icon;
+  button.setAttribute(DATA_ATTRIBUTE, isPinned ? '1' : '0');
+};
+
+const sortSections = () => {
+  const pinnedSections = [];
+  const unpinnedSections = [];
+  const sidebar = getSidebar();
+  const sections = getSections(sidebar);
+
+  sections.forEach((section) => {
+    const button = section.querySelector('button');
+    const hasBeenPinned = getIsButtonCurrentlyPinned(button);
+    if (hasBeenPinned) {
+      pinnedSections.push(section);
+    } else {
+      unpinnedSections.push(section);
+    }
+  });
+
+  [...pinnedSections, ...unpinnedSections].forEach((section) =>
+    sidebar.querySelector('.c-side-menu__list').appendChild(section)
+  );
+};
+
+const togglePin = (event) => {
+  const button = event.currentTarget;
+  const section = button.parentElement;
+  const hasBeenPinned = !getIsButtonCurrentlyPinned(button);
+
+  appendIcon(button, hasBeenPinned);
+
+  const name = getSectionName(section);
+  GM_setValue(name, hasBeenPinned);
+  sortSections();
+};
+
+const insertPinIcon = async (section) => {
+  const button = document.createElement('button');
+  section.style.position = 'relative';
+  button.type = 'button';
+  button.classList.add('spendesk-fav-button');
+  button.addEventListener('click', togglePin);
+
+  const name = getSectionName(section);
+  const isPinned = await GM_getValue(name);
+
+  appendIcon(button, isPinned);
+
+  section.appendChild(button);
+};
+
+const addPinIcons = async (sidebar) => {
+  const sections = getSections(sidebar);
+  for (const section of sections) {
+    await insertPinIcon(section);
+  }
+  sortSections();
+};
+
+const main = async () => {
+  const sidebar = getSidebar();
+  if (sidebar) {
+    return addPinIcons(sidebar);
+  }
+  await wait(100);
+  main();
+};
+
+(async function() {
+  'use strict';
+  await main();
+})();
