6ef2906db0e4ea9e6038a8c066a84dc356c352e1
Bastien Jorge <bastien.jorge@gmail.com>
2020-10-13 11:46:05 +0200

    Fix - Filter - Re allow click on collection (#11)
    * fix: use prepend instead of innerHTML
    * refactor: format with new prettier
    * Bump to 0.2

--- scripts/filter-on-forest.js
+++ scripts/filter-on-forest.js
@@ -3,7 +3,7 @@
 // @namespace    https://spendesk.com
 // @updateURL    https://raw.githubusercontent.com/Spendesk/tampermonkey-scripts/master/scripts/filter-on-forest.js
 // @downloadURL  https://raw.githubusercontent.com/Spendesk/tampermonkey-scripts/master/scripts/filter-on-forest.js
-// @version      0.1
+// @version      0.2
 // @description  Add a filter on forest for the sidebar
 // @author       Spendesk
 // @match        *://app.forestadmin.com/*
@@ -17,39 +17,42 @@ const wait = (timeout) =>
   });
 
 const filter = () => {
-  const filterSearchBar = document.querySelector('#filterSearchBar');
+  const filterSearchBar = document.querySelector("#filterSearchBar");
   const searchTerm = filterSearchBar.value.toLowerCase();
 
-  const sideMenuItems = document.querySelectorAll('.c-side-menu-item');
+  const sideMenuItems = document.querySelectorAll(".c-side-menu-item");
   for (const sideMenuItem of sideMenuItems) {
     const sideMenuItemName = sideMenuItem.innerText.toLowerCase();
     const sideMenuDiv = sideMenuItem.parentElement;
     if (sideMenuItemName.includes(searchTerm)) {
-      sideMenuDiv.style.display = 'block';
+      sideMenuDiv.style.display = "block";
     } else {
-      sideMenuDiv.style.display = 'none';
+      sideMenuDiv.style.display = "none";
     }
   }
 };
 
 const addSidebarSearch = (sidebar) => {
-  const button = `
-<div class='c-beta-header__search-bar c-beta-search-bar ember-view' style='margin-top: 5px; margin-bottom: 5px;'>
-<i class='material-icons c-beta-search-bar__icon' role='button' data-ember-action=''>search</i>
-<form class='c-beta-search-bar__form' data-ember-action=''>
-<input autocomplete='off' placeholder='Filter collection' class='c-beta-search-bar__input ember-text-field ember-view' type='text' id='filterSearchBar'>
-</form>
-</div>`;
+  const htmlSearchbar = `
+  <div class="c-beta-header__search-bar c-beta-search-bar" style="margin-top: 5px; margin-bottom: 5px;">
+  <i class="material-icons c-beta-search-bar__icon" role="button" data-ember-action="">search</i>
+    <form class="c-beta-search-bar__form" data-ember-action="">
+      <input autocomplete="off" placeholder="Filter collection" class="c-beta-search-bar__input ember-text-field ember-view" type="text" id="filterSearchBar">
+    </form>
+  </div>
+`;
 
-  sidebar.innerHTML = button + sidebar.innerHTML;
-  const domButton = document.querySelector('#filterSearchBar');
-  domButton.addEventListener('keyup', filter);
+  const domSearchBarDiv = document.createElement("div");
+  domSearchBarDiv.innerHTML = htmlSearchbar;
+  sidebar.prepend(domSearchBarDiv);
+  const domSearchBar = document.querySelector("#filterSearchBar");
+  domSearchBar.addEventListener("keyup", filter);
 
-  domButton.focus();
+  domSearchBar.focus();
 };
 
 const main = async () => {
-  const sidebar = document.querySelector('.l-sidebar');
+  const sidebar = document.querySelector(".l-sidebar");
   if (sidebar) {
     return addSidebarSearch(sidebar);
   }
@@ -57,7 +60,7 @@ const main = async () => {
   main();
 };
 
-(async function() {
-  'use strict';
+(async function () {
+  "use strict";
   await main();
 })();
